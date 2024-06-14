a06acbe8681bb5be988406aceb4c0e110f32bba7
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-06-14 19:16:04 -0300

    feat: ARM support

--- lib/Linux/Info/SysInfo/CPU.pm
+++ lib/Linux/Info/SysInfo/CPU.pm
@@ -4,18 +4,13 @@ use warnings;
 use Carp       qw(confess);
 use Hash::Util qw(lock_keys);
 use Set::Tiny 0.04;
-use Class::XSAccessor
-  getters => {
-    get_arch     => 'architecture',
-    get_cores    => 'cores',
-    get_threads  => 'threads',
-    get_model    => 'model',
-    get_flags    => 'flags',
-    get_bogomips => 'bogomips',
-    get_bugs     => 'bugs',
-    get_vendor   => 'vendor',
-  },
-  exists_predicates => { has_multithread => 'multithread', };
+use Class::XSAccessor getters => {
+    get_arch        => 'architecture',
+    get_model       => 'model',
+    get_bogomips    => 'bogomips',
+    get_vendor      => 'vendor',
+    get_source_file => 'source_file',
+};
 
 # VERSION
 
@@ -33,8 +28,24 @@ sub _parse {
     confess 'Must be overrided by subclasses';
 }
 
+sub processor_regex {
+    confess 'Must be overrided by subclasses';
+}
+
 sub _parse_list {
     return ( split( /\s+:\s/, shift->{line} ) )[1];
+}
+
+sub _custom_attribs {
+    confess 'Must be overrided by subclasses';
+}
+
+sub get_cores {
+    confess 'Must be overrided by subclasses';
+}
+
+sub get_threads {
+    confess 'Must be overrided by subclasses';
 }
 
 sub _parse_flags {
@@ -62,14 +73,16 @@ sub has_flag {
     return 0;
 }
 
+sub get_flags {
+    my @flags = shift->{flags}->members;
+    return \@flags;
+}
+
 sub new {
     my ( $class, $source_file ) = @_;
     my $self = {
-        multithread  => 0,
         model        => undef,
         processors   => 0,
-        cores        => 0,
-        threads      => 0,
         flags        => Set::Tiny->new,
         architecture => undef,
         bogomips     => 0,
@@ -79,16 +92,17 @@ sub new {
     $source_file = '/proc/cpuinfo'
       unless ( ( defined($source_file) ) and ( $source_file ne '' ) );
 
-    croak "The file $source_file is not available for reading"
+    confess "The file $source_file is not available for reading"
       unless ( -r $source_file );
 
     $self->{source_file} = $source_file;
     bless $self, $class;
+    $self->_custom_attribs;
     $self->_parse;
     $self->_set_hyperthread;
     $self->_set_proc_bits;
     delete $self->{line};
-    lock_keys( %{self} );
+    lock_keys( %{$self} );
     return $self;
 }
 
