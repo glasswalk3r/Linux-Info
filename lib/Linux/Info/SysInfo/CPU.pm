6cc40ccdb1cde787ba488280bcf09b9e15e46fc8
Alceu Rodrigues de Freitas Junior <glasswalk3r@yahoo.com.br>
2024-06-14 02:33:38 -0300

    chore: finished implementation (untested)

--- lib/Linux/Info/SysInfo/CPU.pm
+++ lib/Linux/Info/SysInfo/CPU.pm
@@ -1,16 +1,64 @@
 package Linux::Info::SysInfo::CPU;
 use strict;
 use warnings;
-use Carp       qw(croak);
+use Carp       qw(confess);
 use Hash::Util qw(lock_keys);
-use Class::XSAccessor getters => {};
+use Set::Tiny 0.04;
+use Class::XSAccessor
+  getters => {
+    get_arch     => 'architecture',
+    get_cores    => 'cores',
+    get_threads  => 'threads',
+    get_model    => 'model',
+    get_flags    => 'flags',
+    get_bogomips => 'bogomips',
+    get_bugs     => 'bugs',
+  },
+  exists_predicates => { has_multithread => 'multithread', };
 
 # VERSION
 
 # ABSTRACT: Collects CPU information from /proc/cpuinfo
 
-sub _parse {
+sub _set_proc_bits {
+    confess 'Must be overrided by subclasses';
+}
 
+sub _set_hyperthread {
+    confess 'Must be overrided by subclasses';
+}
+
+sub _parse {
+    confess 'Must be overrided by subclasses';
+}
+
+sub _parse_list {
+    return ( split( /\s+:\s/, shift->{line} ) )[1];
+}
+
+sub _parse_flags {
+    my ( $self, $line ) = @_;
+    $self->{line} = $line;
+    my $value = $self->_parse_list;
+    $self->{flags}->insert( split( /\s/, $value ) );
+    $self->{line} = undef;
+}
+
+sub _parse_bugs {
+    my ( $self, $line ) = @_;
+    $self->{line} = $line;
+    my $value = $self->_parse_list;
+    $self->{bugs}->insert( split( /\s/, $value ) );
+    $self->{line} = undef;
+}
+
+sub has_flag {
+    my ( $self, $flag ) = @_;
+
+    # Set::Tiny uses 1 for truth, undef for false
+    return 0 if ( $self->{flags}->is_empty );
+    return 1 if ( $self->{flags}->has($flag) );
+    return 0;
 }
 
 sub new {
@@ -18,17 +66,28 @@ sub new {
     my $self = {
         multithread  => 0,
         model        => undef,
-        pcpucount    => 0,
-        tcpucount    => 0,
-        cpu_flags    => [],
+        processors   => 0,
+        cores        => 0,
+        threads      => 0,
+        flags        => Set::Tiny->new,
         architecture => undef,
-        bogomips     => undef,
+        bogomips     => 0,
+        bugs         => Set::Tiny->new,
     };
     $source_file = '/proc/cpuinfo'
       unless ( ( defined($source_file) ) and ( $source_file ne '' ) );
 
     croak "The file $source_file is not available for reading"
       unless ( -r $source_file );
+
+    $self->{source_file} = $source_file;
+    bless $self, $class;
+    $self->_parse;
+    $self->_set_hyperthread;
+    $self->_set_proc_bits;
+    delete $self->{line};
+    lock_keys( %{self} );
+    return $self;
 }
 
 1;
--- lib/Linux/Info/SysInfo/CPU/Intel.pm
+++ lib/Linux/Info/SysInfo/CPU/Intel.pm
@@ -1,12 +1,102 @@
 package Linux::Info::SysInfo::CPU::Intel;
 use strict;
 use warnings;
-use Carp       qw(croak);
-use Hash::Util qw(lock_keys);
+use Carp       qw(confess);
+use Hash::Util qw(lock_keys unlock_keys);
 use Class::XSAccessor getters => {};
 
 # VERSION
 
-# ABSTRACT: Collects CPU information from /proc/cpuinfo
+# ABSTRACT: Collects Intel based CPU information from /proc/cpuinfo
+
+sub _parse {
+    my $self = shift;
+    my $file = $self->{source_file};
+
+    # model name      : Intel(R) Core(TM) i5-4300M CPU @ 2.60GHz
+    my $model_regex     = qr/^model\sname\s+\:\s(.*)/;
+    my $processor_regex = qr/^physical\s+id\s*:\s*(\d+)/;
+    my $core_regex      = qr/^core\s+id\s*:\s*(\d+)/;
+    my $thread_regex    = qr/^processor\s*:\s*\d+/;
+    my $flags_regex     = qr/^flags\s+\:/;
+
+    # bogomips        : 4784.43
+    my $bogo_regex = qr/^bogomips\s+\:\s(\d+\.\d+)/;
+
+    # bugs            : apic_c1e spectre_v1 spectre_v2 spec_store_bypass
+    my $bugs_regex = qr/^bugs\s+\:\s/;
+    my %processors;
+    my $threads       = 0;
+    my $flags_defined = 0;
+    my $bugs_defined  = 0;
+    my $phyid;
+    open( my $fh, '<', $file ) or confess "Cannot read $file: $!";
+
+  LINE: while ( my $line = <$fh> ) {
+        chomp($line);
+
+        if ( $line =~ $bogo_regex ) {
+            $self->{bogomips} = $1;
+        }
+
+        if ( $line =~ $processor_regex ) {
+
+            # in order for this to work, it is expected that the physical line
+            # comes first than the core
+            $phyid = $1;
+            $processors{$phyid}->{count}++;
+            next LINE;
+        }
+
+        if ( $line =~ $core_regex ) {
+            $processors{$phyid}->{cores}{$1}++;
+            next LINE;
+        }
+
+        if ( $line =~ $thread_regex ) {
+            $threads++;
+            next LINE;
+        }
+
+        if ( $line =~ $bugs_regex ) {
+            next LINE if ($bugs_defined);
+            $self->_parse_bugs($line);
+            $bugs_defined = 1;
+        }
+
+        if ( $line =~ $flags_regex ) {
+            next LINE if ($flags_defined);
+            $self->_parse_flags($line);
+            $flags_defined = 1;
+        }
+    }
+
+    close($fh);
+    $self->{processors} = scalar( keys(%processors) );
+    $self->{cores}      = $processors{0}->{cores};
+    $self->{threads}    = $threads;
+}
+
+sub _set_proc_bits {
+    my $self = shift;
+
+    if ( $self->has_flag('lm') ) {
+        $self->{architecture} = 64;
+    }
+    else {
+        $self->{architecture} = 32;
+    }
+}
+
+sub _set_hyperthread {
+    my $self = shift;
+
+    if ( $self->has_flag('ht') ) {
+        $self->{multithread} = 1;
+    }
+    else {
+        $self->{multithread} = 0;
+    }
+}
 
 1;
